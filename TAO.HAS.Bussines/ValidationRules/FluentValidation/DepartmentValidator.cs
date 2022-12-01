321afe25849b3694bc6c55fa277d3ccc7bfe4329
Moongazing <tunali_2@outlook.com>
2022-12-01 23:17:31 +0300

    Adding Department,Location,Proffesion Validator

---
+++ TAO.HAS.Bussines/ValidationRules/FluentValidation/DepartmentValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TAO.HAS.Entities.Concrete;
+
+namespace TAO.HAS.Business.ValidationRules.FluentValidation
+{
+  public class DepartmentValidator:AbstractValidator<Department>
+  {
+    public DepartmentValidator()
+    {
+      RuleFor(d => d.Description).NotEmpty();
+      RuleFor(d => d.Description).MinimumLength(5).MinimumLength(30);
+    }
+  }
+}
---
+++ TAO.HAS.Bussines/ValidationRules/FluentValidation/LocationValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TAO.HAS.Entities.Concrete;
+
+namespace TAO.HAS.Business.ValidationRules.FluentValidation
+{
+  public class LocationValidator:AbstractValidator<Location>
+  {
+    public LocationValidator()
+    {
+      RuleFor(l=>l.City).NotEmpty();
+      RuleFor(l => l.Country).NotEmpty();
+      RuleFor(l => l.Description).NotEmpty();
+    }
+  }
+}
